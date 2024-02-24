e0a8a634987c5cab353b32d8e41ead01e9e06c30
Koh-Du-Beom <140967208+Koh-Du-Beom@users.noreply.github.com>
2024-02-24 16:37:45 +0900

    first commit

---
+++ src/types/ActivityType.type.ts
@@ -0,0 +1,14 @@
+interface ActivityType{
+	id : string;
+	pageType : string,
+	activityImg : string,
+	program: string,
+	type : string,
+	topic : string, 
+	point : number,
+	agency : string,
+	date : string,
+	detail : string,
+}
+
+export default ActivityType;
---
+++ src/types/UserInfoType.type.ts
@@ -0,0 +1,15 @@
+interface UserInfoType{
+	name : string;
+	grade : string;
+	major : string;
+	studentNumber : string;
+	phoneNumber : string;
+	email : string;
+	bankAccount : string;
+	bankName : string;
+	bankBookImg : string;
+	idCardImg : string;
+	signImg : string;
+}
+
+export default UserInfoType;
